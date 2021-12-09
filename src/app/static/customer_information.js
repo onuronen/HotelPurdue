b3c9d72c85cd3b79ea79d5064eafc62c841db16c
marksium <bogdanov@purdue.edu>
2021-12-08 21:16:04 -0500

    Look up Reservation/Customer, generate bill

--- src/app/static/customer_information.js
+++ src/app/static/customer_information.js
@@ -19,6 +19,18 @@ document.querySelector("#customer_info_lookup").addEventListener("click", functi
         }
     })
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => {
+        console.log(data)
+        document.querySelector("#full_names").innerText=data.full_name
+        document.querySelector("#cust_number").innerText=data.cust_number
+        document.querySelector("#cust_email").innerText=data.cust_email
+        document.querySelector("#cust_street").innerText=data.cust_street + ', ' + data.cust_state + ', ' + data.cust_country
+        document.querySelector("#cust_ids").innerText=data.id_number
+        document.querySelector("#doc_types").innerText=data.doc_type
+        document.querySelector("#card_numbers").innerText=data.card_number
+        document.querySelector("#exp_dates").innerText=data.exp_date
+        document.querySelector("#cvv_nums").innerText="************" + data.cvv_num
+        document.querySelector("#notess").innerText=data.notes
+    });
 
 })
--- src/app/static/reservation_lookup.js
+++ src/app/static/reservation_lookup.js
@@ -19,6 +19,23 @@ document.querySelector("#res_lookup").addEventListener("click", function(evt) {
         }
     })
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => {
+        console.log(data)
+        //console.log(data.end_date)
+        document.querySelector("#full_name").innerText=data.full_name
+        document.querySelector("#guest_number").innerText=data.guest_number
+        document.querySelector("#room_numbers").innerText=data.room_number
+        //console.log(data.room_number)
+        document.querySelector("#room_type").innerText=data.room_type
+        document.querySelector("#start_date").innerText=data.start_date
+        document.querySelector("#end_date").innerText=data.end_date
+        document.querySelector("#reservation_date").innerText=data.reservation_date
+        document.querySelector("#notes").innerText=data.notes
+        //document.querySelector("#room_id").innerText=data['0'].room_id
+        //document.querySelector("#room_type").innerText=data['0'].room_type
+        //document.querySelector("#room_number").innerText=data['0'].room_number
+        //document.querySelector("#room_rate").innerText=data['0'].price
+        //document.querySelector("#room_occupancy").innerText=data['0'].max_occupancy
+    });
 
 })
