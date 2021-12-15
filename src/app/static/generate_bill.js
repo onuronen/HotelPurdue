59456f48a3026e223569f4b8b8c2743d1aa549e2
marksium <bogdanov@purdue.edu>
2021-12-14 21:31:13 -0500

    fixed pages

--- src/app/static/generate_bill.js
+++ src/app/static/generate_bill.js
@@ -21,6 +21,27 @@ document.querySelector("#customer_check_out").addEventListener("click", function
     .then(response => response.json())
     .then(data => {
         console.log(data)
+        document.querySelector("#full_names").innerText=data.cust_fname + " " + data.cust_lname
+        document.querySelector("#emails").innerText=data.cust_email
+        document.querySelector("#phone_numbers").innerText=data.cust_number
+        document.querySelector("#streets").innerText=data.cust_street
+        document.querySelector("#states").innerText=data.cust_state
+        document.querySelector("#countrys").innerText=data.cust_country
+        document.querySelector("#id_numbers").innerText=data.customer_id
+        document.querySelector("#id_types").innerText=data.doc_type
+        document.querySelector("#card_numbers").innerText="************" + data.id_number
+        
+        
+        document.querySelector("#room_numbers").innerText=data.room_number
+        document.querySelector("#room_types").innerText=data.room_type
+        //document.querySelector("#check_ins").innerText=data.
+        //document.querySelector("#check_outs").innerText=data.cust_state
+
+        document.querySelector("#room_charges").innerText=data.room_charge
+        //document.querySelector("#bar_charges").innerText=data.room_type
+        //document.querySelector("#add_charges").innerText=data.
+        document.querySelector("#taxes").innerText=data.tax
+        document.querySelector("#totals").innerText=data.total
     });
 
 })
